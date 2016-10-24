4f7b0e365404d70c75ca456562cc91b24cb336bc
Kotar Creative <kotarcreative@gmail.com>
2016-10-24 00:17:19 -0600

    Add lato font to emails.

--- resources/views/emails/admin/report-bootmark.blade.php
+++ resources/views/emails/admin/report-bootmark.blade.php
@@ -1,8 +1,8 @@
-<h1>A bootmark was reported</h1>
+<h1 style='font-family: "Lato" Open sans;font-weight: 600'>A bootmark was reported</h1>
 
 <p>The bootmark was reported by the user with id: {{ $reporter_id }}</p>
 
-<h3>Report Information</h3>
+<h3 style='font-family: "Lato" Open sans;font-weight: 400'>Report Information</h3>
 <ul style="list-style-type: none">
     <li>Report ID: {{ $id }}</li>
     <li>Bootmark ID: {{ $bootmark_id }}</li>
--- resources/views/emails/users/report-bootmark.blade.php
+++ resources/views/emails/users/report-bootmark.blade.php
@@ -1,14 +1,16 @@
-<h1>Your report has been delivered</h1>
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+
+<h1 style='font-family: "Lato" Open sans;font-weight: 600' >Your report has been delivered</h1>
 
 <p>We appreciate your contribution to making Bootmark the very best it can be.</p>
 
-<h3>Our commitment to quality content</h3>
+<h3 style='font-family: "Lato" Open sans;font-weight: 400'>Our commitment to quality content</h3>
 <p>We believe in creating a community that consistently posts high quality, interesting content. There is no room for offensive or malicious content, and we work hard to ensure that this content is taken down as soon as it is reported.</p>
 
-<h3>We aim for quality experiences</h3>
+<h3 style='font-family: "Lato" Open sans;font-weight: 400'>We aim for quality experiences</h3>
 <p>The higher quality the content, the better and more uplifting the experience for you as the user. We love people that make an earnest effort to maintain a positive experience for other users. Users that have multiple posts reported as being offensive or malicious will be reviewed and may be banned from the app if necessary.</p>
 
-<h3>Thanks again</h3>
+<h3 style='font-family: "Lato" Open sans;font-weight: 400'>Thanks again</h3>
 <p>By reporting offensive and malicious content you are playing an important role in keeping this app awesome. You rock.</p>
 
 <span>Mike|Scott|Dave|Isaac|Cody</span>
