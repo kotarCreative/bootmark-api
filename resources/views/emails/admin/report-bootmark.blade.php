c13cd9a5da86f9b7e7bbe0d4d8ddcef438a4ae95
Kotar Creative <kotarcreative@gmail.com>
2016-10-23 23:50:25 -0600

    Fix bootmark id for bootmark report.

--- resources/views/emails/admin/report-bootmark.blade.php
+++ resources/views/emails/admin/report-bootmark.blade.php
@@ -5,7 +5,7 @@
 <h3>Report Information</h3>
 <ul style="list-style-type: none">
     <li>Report ID: {{ $id }}</li>
-    <li>Comment ID: {{ $comment_id }}</li>
+    <li>Bootmark ID: {{ $bootmark_id }}</li>
     <li>Message: {{ $bodyMessage }}</li>
     <li>Current Status: {{ $status }}</li>
 </ul>
