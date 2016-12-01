ce031196fb9c947369fae401187fee6cecdde4a8
Kotar Creative <kotarcreative@gmail.com>
2016-11-30 20:47:40 -0700

    Fix syntax for earth distance function.

--- app/Http/Controllers/BootmarkController.php
+++ app/Http/Controllers/BootmarkController.php
@@ -53,7 +53,7 @@ class BootmarkController extends Controller
         /* Applies the filter that has been selected */
         $distance_select = '';
         if($request->input('filter') == 'closest') {
-            $distance_select = "earthdistance(ll_to_earth($lat,$lng), ll_to_earth(lat, lng)) as distance_from_current";
+            $distance_select = "earth_distance(ll_to_earth($lat,$lng), ll_to_earth(lat, lng)) as distance_from_current";
             $bootmarks = $bootmarks->orderBy('distance_from_current', 'asc');
             /* Find bootmarks in a radius */
             //$bootmarks->whereRaw("earth_box(ll_to_earth($lat,$lng), $rad) @> ll_to_earth(lat, lng)");
