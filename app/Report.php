39b8458184725f5a3b36886cbfbfbf1cbd19b768
David Buss <kotarcreative@gmail.com>
2016-12-08 14:52:22 -0700

    Hotfix for getEnums()

--- app/Report.php
+++ app/Report.php
@@ -22,8 +22,6 @@ class Report extends Model
      * @return array Returns an array of the enums from the reports table
      */
     public static function getEnums() {
-        $type = DB::select(DB::raw("SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'reports' AND column_name LIKE 'reason'"));
-        preg_match("/^enum\\(\\'(.*)\\'\\)$/", $type[0]->COLUMN_TYPE, $matches);
-        return explode("','", $matches[1]);
+        return ['spam', 'inappropriate'];
     }
 }
