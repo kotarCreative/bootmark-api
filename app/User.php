d4b9e3aa772b62b3bbcc4e3d914d916592e1fd83
Kotar Creative <kotarcreative@gmail.com>
2017-01-31 21:43:15 -0700

    Change the relation on the followers -> users relations.

--- app/User.php
+++ app/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
      */
     public function followers()
     {
-	    return $this->hasMany('App\Follower');
+	    return $this->belongsToMany('App\User', 'App\Follower', 'follower_id', 'user_id');
     }
 
     /**
@@ -68,6 +68,6 @@ class User extends Authenticatable
      */
     public function following()
     {
-        return $this->belongsTo('App\Follower');
+        return $this->belongsToMany('App\User', 'App\Follower', 'user_id', 'follower_id');
     }
 }
