586aaa939df59139a63e54e40392eb969d1d5322
Cody Moorhouse <cody.moorhouse@icloud.com>
2017-05-22 19:30:40 +0000

    Add created at field

--- app/Http/Controllers/BootmarkController.php
+++ app/Http/Controllers/BootmarkController.php
@@ -174,7 +174,7 @@ class BootmarkController extends Controller
                 $se_lng = $this->calc_coord($north_west["lng"], $grid_width, $j + 1);
 
                 /* ST_MakeEnvelope(LEFT, BOTTOM, RIGHT, TOP, SRID) -- https://gis.stackexchange.com/questions/25797/select-bounding-box-using-postgis */
-                $grid_query = Bootmark::selectRaw("bootmarks.id, users.id as user_id, users.name, description, location, lat, lng, discoverable, type")
+                $grid_query = Bootmark::selectRaw("bootmarks.id, users.id as user_id, users.name, description, location, lat, lng, discoverable, type, bootmarks.created_at")
                                ->whereExists(function($query) use ($nw_lat, $nw_lng, $se_lat, $se_lng) {
                                    if ($nw_lng > $se_lng) {
                                         $envelope_1 = "ST_MakeEnvelope($nw_lng, $se_lat, 180, $nw_lat, 4326)";
